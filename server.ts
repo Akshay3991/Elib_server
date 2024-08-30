287cb6cdab8a39c646914b051ff494d19a04b2ea
Akshay <akshaysabharwal92@mail.com>
2024-08-30 17:24:26 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+import app from "./src/app";
+import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
+
+const startServer = async () => {
+    // connect database
+    await connectDB();
+    const port = config.port || 3000;
+
+
+    app.listen(port, () => {
+        console.log(`listening on port : ${port}`)
+    });
+
+};
+
+startServer();  
